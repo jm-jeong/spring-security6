eacb9bc254b81ddf6774af3d6e4fa366f1f68703
jaemin <jjm7655@gmail.com>
2024-07-31 17:53:45 +0900

    feat: SecurityConfig 내 Authority 별 접근할 수 있는 리소스 설정
    hasAuthority : 1개 권한
    hasAnyAuthority : 여러개 권한

--- src/main/java/com/ezybytes/springsecurity6/config/ProjectSecurityConfig.java
+++ src/main/java/com/ezybytes/springsecurity6/config/ProjectSecurityConfig.java
@@ -37,7 +37,7 @@ public class ProjectSecurityConfig {
 	 */
 	@Bean
 	public AuthenticationManager authenticationManager() throws Exception {
-		ProviderManager providerManager = (ProviderManager) authenticationConfiguration.getAuthenticationManager();
+		ProviderManager providerManager = (ProviderManager)authenticationConfiguration.getAuthenticationManager();
 		providerManager.getProviders().add(this.eazyBankUsernamePwdAuthenticationProvider);
 		return authenticationConfiguration.getAuthenticationManager();
 	}
@@ -50,23 +50,29 @@ public class ProjectSecurityConfig {
 
 		//아래 2줄을 통해서 서로 다른 Origin에서 첫 로그인이 완료되면 항상 JSESSIONID가 생성되고 동일한 JSESSIONID가 UI 앱에 보내지고 UI 앱은 첫 로그인 후에 만들어지는 후속 요청들을 활용할 수 있게 해줌
 		//만약 아래 2줄 선언 안하면 매번 보안된 api 접근할 때마다 Angular 앱에서 자격증명을 입력해야함
-		http.securityContext((securityContext) -> securityContext.requireExplicitSave(false))//spring security 프레임워크에서 SecurityContextHolder안에 있는 인증 정보들을 저장하는 역할을 맡지 않고, 프레임워크들이 대신 수행하게 함 기본설정은 true
+		http.securityContext((securityContext) -> securityContext.requireExplicitSave(
+				false))//spring security 프레임워크에서 SecurityContextHolder안에 있는 인증 정보들을 저장하는 역할을 맡지 않고, 프레임워크들이 대신 수행하게 함 기본설정은 true
 			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
 			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			// .csrf((csrf) -> csrf.ignoringRequestMatchers("/contact", "/register"))//authorizeHttpRequests보다 앞에 선언되어야 작동함. csrf 제외해야 하는 데이터를 보내는 메서드인 post put 등의 request
-			.csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers( "/register")//_csrf 토큰 심기 위해서 선언
-				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))//쿠키 방식으로 토큰 저장, withHttpOnlyFalse()는 JavaScript에서도 사용하기 위해서
-			.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)//BasicAuthenticationFilter 이후에 CsrfCookieFilter 실행, 즉 로그인 완료후 CSRF 토큰 생성해서 응답 값에 채움
+			.csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler)
+				.ignoringRequestMatchers("/register")//_csrf 토큰 심기 위해서 선언
+				.csrfTokenRepository(
+					CookieCsrfTokenRepository.withHttpOnlyFalse()))//쿠키 방식으로 토큰 저장, withHttpOnlyFalse()는 JavaScript에서도 사용하기 위해서
+			.addFilterAfter(new CsrfCookieFilter(),
+				BasicAuthenticationFilter.class)//BasicAuthenticationFilter 이후에 CsrfCookieFilter 실행, 즉 로그인 완료후 CSRF 토큰 생성해서 응답 값에 채움
 			.authorizeHttpRequests((authorize) -> authorize
-			.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards", "/user","/contact").authenticated()
-			.requestMatchers( "/notices", "/register", "/v3/**", "/swagger-ui/**").permitAll())
+				.requestMatchers(("/myAccount")).hasAuthority("VIEWACCOUNT")
+				.requestMatchers(("/myLoans")).hasAuthority("VIEWLOANS")
+				.requestMatchers(("/myCards")).hasAuthority("VIEWCARDS")
+				.requestMatchers(("/myBalance")).hasAnyAuthority("VIEWACCOUNT", "VIEWBALANCE")
+				.requestMatchers("/user").authenticated()
+				.requestMatchers("/notices", "/register", "/v3/**", "/swagger-ui/**").permitAll())
 			.formLogin(Customizer.withDefaults())
 			.httpBasic(Customizer.withDefaults());
 
 		return http.build();
 	}
-
-
 
 	/*
 	pre-flight: 서버로 바로 요청을 보내는 Simple Request와는 다르게, 지금 보내는 요청이 유효한지를 확인하기 위해 OPTIONS 메서드로 예비 요청을 보내는 것
@@ -85,11 +91,10 @@ public class ProjectSecurityConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
 
-
 		return source;
 	}
 
-/*	*//**
+	/*	*//**
 	 * NoOpPasswordEncoder is not recommended for production usage.
 	 * Use only for non-prod.
 	 * 해싱안하고 텍스트로 비번 저장
