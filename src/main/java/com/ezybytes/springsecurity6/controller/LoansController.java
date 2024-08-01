51b8ba9433b359dcd4dff334b65ed32f7da00542
jaemin <jjm7655@gmail.com>
2024-08-02 00:44:37 +0900

    feat: @PostAuthorize 예제, 해당 메소드 실행될 때 해당 권한 가지고 있는지 체크

--- src/main/java/com/ezybytes/springsecurity6/controller/LoansController.java
+++ src/main/java/com/ezybytes/springsecurity6/controller/LoansController.java
@@ -1,6 +1,7 @@
 package com.ezybytes.springsecurity6.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class LoansController {
     @Autowired
     private LoanRepository loanRepository;
 
+    @PostAuthorize("hasAnyRole('USER')")
     @GetMapping("/myLoans")
     public List<Loans> getLoanDetails(@RequestParam int id) {
         List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
